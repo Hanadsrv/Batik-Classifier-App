1d695243837a845909edf7c7340f07b2e9c6d646
Hana Desriva <hana.desriva@student.president.ac.id>
2023-04-03 15:28:30 +0700

    Batik Classifier

--- app/src/main/java/com/example/batikclassifierapplication_001202000129/MainActivity.kt
+++ app/src/main/java/com/example/batikclassifierapplication_001202000129/MainActivity.kt
@@ -2,8 +2,17 @@ package com.example.batikclassifierapplication_001202000129
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var imageView: ImageView? = null
+    var logout_btn: ImageView? = null
+    var result: TextView? = null
+    var percentage: TextView? = null
+    private val imageSize = 224
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
