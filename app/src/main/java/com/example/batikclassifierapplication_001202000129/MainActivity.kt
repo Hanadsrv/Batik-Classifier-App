06fccfa915582c18d51fd641d76a7c0f9844358c
Hana Desriva <hana.desriva@student.president.ac.id>
2023-04-04 15:37:00 +0700

    Batik Classifier

--- app/src/main/java/com/example/batikclassifierapplication_001202000129/MainActivity.kt
+++ app/src/main/java/com/example/batikclassifierapplication_001202000129/MainActivity.kt
@@ -1,9 +1,24 @@
 package com.example.batikclassifierapplication_001202000129
 
+import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.media.ThumbnailUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Process
+import android.provider.MediaStore
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.example.batikclassifierapplication_001202000129.ml.BatikModel
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,10 +26,148 @@ class MainActivity : AppCompatActivity() {
     var logout_btn: ImageView? = null
     var result: TextView? = null
     var percentage: TextView? = null
+
     private val imageSize = 224
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar!!.hide()
+
+        imageView = findViewById(R.id.imageView)
+        logout_btn = findViewById(R.id.logout_btn)
+        result = findViewById(R.id.result)
+        percentage = findViewById(R.id.percentage)
+
+        val camera = findViewById<LinearLayout>(R.id.camera_btn)
+        val gallery = findViewById<LinearLayout>(R.id.gallery_btn)
+        val live = findViewById<LinearLayout>(R.id.live_btn)
+
+        camera.setOnClickListener{
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, 3)
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
+            }
+        }
+
+        gallery.setOnClickListener{
+            val cameraIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(cameraIntent, 1)
+        }
+
+        live.setOnClickListener{
+            val intent = Intent(this@MainActivity, realTime::class.java)
+            startActivity(intent)
+        }
+
+        fun classifyImage(image: Bitmap?) {
+            try {
+                val model = BatikModel.newInstance(applicationContext)
+                val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+                val byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
+                byteBuffer.order(ByteOrder.nativeOrder())
+                val intValues = IntArray(imageSize * imageSize)
+                image!!.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
+                var pixel = 0
+                for (i in 0 until imageSize) {
+                    for (j in 0 until imageSize) {
+                        val `val` = intValues[pixel++] // RGB
+                        byteBuffer.putFloat((`val` shr 16 and 0xFF) * (1f / 1))
+                        byteBuffer.putFloat((`val` shr 8 and 0xFF) * (1f / 1))
+                        byteBuffer.putFloat((`val` and 0xFF) * (1f / 1))
+                    }
+                }
+
+                inputFeature0.loadBuffer(byteBuffer)
+
+                // Menjalankan model dan mendapatkan hasil
+                val outputs = model.process(inputFeature0)
+                val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+                val confidences = outputFeature0.floatArray
+                // Mencari indeks kelas dengan nilai confidence terbesar
+                var maxPos = 0
+                var maxConfidence = 0f
+                for (i in confidences.indices) {
+                    if (confidences[i] > maxConfidence) {
+                        maxConfidence = confidences[i]
+                        maxPos = i
+                    }
+                }
+                val classes = arrayOf(
+                    "Batik Bali",
+                    "Batik Betawi",
+                    "Batik Celup",
+                    "Batik Cendrawasih",
+                    "Batik Dayak",
+                    "Batik Geblek Renteng",
+                    "Batik Insang",
+                    "Batik Kawung",
+                    "Batik Lasem",
+                    "Batik Megamendung",
+                    "Batik Pala",
+                    "Batik Parang",
+                    "Batik Poleng",
+                    "Batik Sekar Jagad",
+                    "Batik Tambal"
+                )
+                result!!.text = classes[maxPos]
+                println("Confidence: $maxConfidence")
+                percentage!!.text = String.format("%.2f", maxConfidence)
+
+                // Close model jika tidak lagi digunakan
+                model.close()
+            } catch (e: IOException) {
+
+            }
+        }
+
+        fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            if (resultCode == RESULT_OK) {
+                // Pengambilan gambar untuk fitur kamera
+                if (requestCode == 3) {
+                    var image = data!!.extras!!["data"] as Bitmap?
+                    val dimension = Math.min(image!!.width, image.height)
+                    image = ThumbnailUtils.extractThumbnail(image, dimension, dimension)
+                    imageView!!.setImageBitmap(image)
+                    image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false)
+                    classifyImage(image)
+
+                    // Pengambilan gambar untuk fitur galeri
+                } else {
+                    val dat = data!!.data
+                    var image: Bitmap? = null
+                    try {
+                        image = MediaStore.Images.Media.getBitmap(this.contentResolver, dat)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                    imageView!!.setImageBitmap(image)
+                    image = Bitmap.createScaledBitmap(image!!, imageSize, imageSize, false)
+                    classifyImage(image)
+                }
+            }
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
+        fun onBackPressed() {
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            alertDialogBuilder.setTitle("Exit Application?")
+            alertDialogBuilder
+                .setMessage("Click yes to exit!")
+                .setCancelable(false)
+                .setPositiveButton(
+                    "Yes"
+                ) { dialog, id ->
+                    moveTaskToBack(true)
+                    Process.killProcess(Process.myPid())
+                    System.exit(1)
+                }
+                .setNegativeButton("No") { dialog, id -> dialog.cancel() }
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+        }
     }
 }
---
+++ app/src/main/java/com/example/batikclassifierapplication_001202000129/realTime.kt
@@ -0,0 +1,216 @@
+package com.example.batikclassifierapplication_001202000129
+
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.view.PreviewView
+import com.example.batikclassifierapplication_001202000129.ml.BatikModel
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.*
+import android.media.Image
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.util.concurrent.ExecutionException
+
+class realTime : AppCompatActivity() {
+    var img_preview: PreviewView? = null
+    var results: TextView? = null
+
+    var IMG_SIZE = 224
+    var model: BatikModel? = null
+
+    private val REQUEST_CODE_PERMISSIONS = 123
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.real_time_detection)
+        supportActionBar!!.hide()
+        img_preview = findViewById(R.id.viewRealTime)
+        results = findViewById(R.id.result)
+
+        try {
+            model = BatikModel.newInstance(
+                applicationContext
+            )
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        if (allPermissionGranted()) {
+            openCamera()
+        } else {
+            ActivityCompat.requestPermissions(this,REQUIRED_PERMISSIONS,REQUEST_CODE_PERMISSIONS)
+        }
+    }
+
+    private fun openCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(
+            {
+                try {
+                    val cameraProvider = cameraProviderFuture.get()
+                    bindPreview(cameraProvider)
+                }
+                catch (e:ExecutionException) {}
+                catch (e:InterruptedException) {}
+                catch (e:IOException) {
+                    e.printStackTrace()
+                }
+            }, ActivityCompat.getMainExecutor(this)
+        )
+    }
+
+    fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val builder = ImageCapture.Builder()
+        val imageCapture = builder.build()
+        val preview = Preview.Builder().build()
+        preview.setSurfaceProvider(img_preview!!.surfaceProvider)
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+        val imageAnalysis = ImageAnalysis.Builder().setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build()
+
+        imageAnalysis.setAnalyzer(ActivityCompat.getMainExecutor(this)) {
+            image -> var result: String? = null
+            @SuppressLint("UnsafeOptInUsageError") val img = image.image
+
+            try {
+                result = classifyImage(img, model)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            results!!.text = result
+            image.close()
+        }
+
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis, imageCapture)
+    }
+
+    private fun classifyImage(image: Image?, model: BatikModel?): String? {
+        var img = toBitmap(image!!)
+        img = Bitmap.createScaledBitmap(img, 224, 224, false)
+
+        try {
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+            val byteBuffer = ByteBuffer.allocateDirect(4*IMG_SIZE*IMG_SIZE*3)
+            byteBuffer.order(ByteOrder.nativeOrder())
+            val intValues = IntArray(IMG_SIZE*IMG_SIZE)
+            img.getPixels(intValues, 0, img.width, 0, 0, img.width, img.height)
+            var pixel = 0
+
+            for (i in 0 until IMG_SIZE) {
+                for (j in 0 until IMG_SIZE) {
+                    val `val` = intValues[pixel++]
+                    byteBuffer.putFloat((`val` shr 16 and 0xff)*(1f/1))
+                    byteBuffer.putFloat((`val` shr 8 and 0xff)*(1f/1))
+                    byteBuffer.putFloat((`val` and 0xff)*(1f/1))
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer)
+
+            val outputs = model!!.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+            val percentages = outputFeature0.floatArray
+
+            var maxPos = 0
+            var maxPercetage = 0f
+
+            for (i in percentages.indices) {
+                if (percentages[i] > maxPercetage) {
+                    maxPercetage = percentages[i]
+                    maxPos = 1
+                }
+            }
+
+            val classes = arrayOf(
+                "Batik Bali",
+                "Batik Betawi",
+                "Batik Celup",
+                "Batik Cendrawasih",
+                "Batik Dayak",
+                "Batik Geblek Renteng",
+                "Batik Insang",
+                "Batik Kawung",
+                "Batik Lasem",
+                "Batik Megamendung",
+                "Batik Pala",
+                "Batik Parang",
+                "Batik Poleng",
+                "Batik Sekar Jagad",
+                "Batik Tambal"
+            )
+
+            Log.d("classify", classes[maxPos])
+            return classes[maxPos] + " " + String.format("%.2f", maxPercetage) + "%"
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    private fun allPermissionGranted(): Boolean {
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                return false
+            }
+        }
+        return true
+    }
+
+    fun toBitmap(image: Image): Bitmap {
+        val planes = image.planes
+        val yBuffer = planes[0].buffer
+        val uBuffer = planes[1].buffer
+        val vBuffer = planes[2].buffer
+        val ySize = yBuffer.remaining()
+        val uSize = uBuffer.remaining()
+        val vSize = vBuffer.remaining()
+        val nv21 = ByteArray(ySize + uSize + vSize)
+        //U and V are swapped
+        yBuffer[nv21, 0, ySize]
+        vBuffer[nv21, ySize, vSize]
+        uBuffer[nv21, ySize + vSize, uSize]
+        val yuvImage = YuvImage(nv21, ImageFormat.NV21, image.width, image.height, null)
+        val out = ByteArrayOutputStream()
+        yuvImage.compressToJpeg(Rect(0, 0, yuvImage.width, yuvImage.height), 100, out)
+        val imageBytes = out.toByteArray()
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionGranted()) {
+                openCamera()
+            } else {
+                Toast.makeText(this, "Camera access denied", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        model!!.close()
+        finish()
+    }
+}
